9f1f0eeac037b1522de76662441946a8735eae25
mukamisha <mukamishajacky97@gmail.com>
2019-10-10 17:25:18 +0200

    changing models

--- Gallery_jacky/settings.py
+++ Gallery_jacky/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'bootstrap3',
-    'gallery_mukamisha.apps.gallery_mukamishaConfig',
+    'gallery_mukamisha',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- gallery_mukamisha/models.py
+++ gallery_mukamisha/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
+# Create your models here.
+
 class Image(models.Model):
     img_name = models.CharField(max_length =30)
     img_description = models.CharField(max_length =30)
-    # email = models.EmailField()
-# Create your models here.
+    # location = models.ForeignKey(Location)
+    # category = models.ForeignKey(Category)
+  
+    def __str__(self):
+        return self.first_name
+
+
+    class Meta:
+        ordering = ['img_name']
