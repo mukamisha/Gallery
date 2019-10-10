8c2eeeaa8e9c0fb920ec8eb486fcc76dc14804cd
mukamisha <mukamishajacky97@gmail.com>
2019-10-10 12:02:37 +0200

    changing my base.html

--- Gallery_jacky/settings.py
+++ Gallery_jacky/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap3',
     'gallery_mukamisha.apps.NewsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
