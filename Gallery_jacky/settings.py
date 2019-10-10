c7f32540938a939be39968c69721ff2f627d9213
mukamisha <mukamishajacky97@gmail.com>
2019-10-10 11:23:52 +0200

    adding the first template

--- Gallery_jacky/settings.py
+++ Gallery_jacky/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gallery_mukamisha.apps.NewsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
