46ff8d138d5d3a162a502eff163c164236d531f6
mukamisha <mukamishajacky97@gmail.com>
2019-10-11 11:37:16 +0200

    method in views.py

--- gallery_mukamisha/views.py
+++ gallery_mukamisha/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse
+from .models import Image,Category,Location
 
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to the Moringa Tribune')
+
+
+def iamges(request):
+   images = Image.objects.all()
+   return render(request, 'templates/image.html',{'images':images})
