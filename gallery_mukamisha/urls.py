d8a76e0e86b60fd620bb39de8cfd37f53852ccd9
mukamisha <mukamishajacky97@gmail.com>
2019-10-11 15:09:32 +0200

    updating url

--- gallery_mukamisha/urls.py
+++ gallery_mukamisha/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns=[
   
      url(r'^$',views.images,name = 'pictures')
-    
+     url(r'^search/', views.search_results, name='search_results')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
